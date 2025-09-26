ee557ee5d883522889ea0e517baf95200883c598
waiphyoaung <waiphyoaung@evercomm.services>
2025-09-26 10:34:04 +0630

    Revert "update"
    This reverts commit 195220d5fe5e1e6f56669b8eb830c0374432cea1.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -56,7 +56,8 @@ keymap.set('n', 'te', ':tabedit<Return>')
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
--- Window navigation: use <C-h/j/k/l> and tabs instead of mapping <Space>
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', '<C-h>', '<C-w>h')
 keymap.set('', '<C-k>', '<C-w>k')
 keymap.set('', '<C-j>', '<C-w>j')
@@ -70,6 +71,8 @@ keymap.set('n', 'tb', ':Gitsigns toggle_current_line_blame<Return>')
 keymap.set('n', '<C-t>', '<Cmd>execute v:count . "ToggleTerm"<CR>', { silent = true })
 keymap.set('t', '<C-t>', "<Esc><Cmd>ToggleTerm<CR>", { silent = true })
 keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
+keymap.set("n", "<C-s>", "<Esc>v:lua.split_term()", { expr = true })
 
 keymap.set("n", "fm", ":lua vim.lsp.buf.format()<Return>")
 keymap.set("v", "fm", vim.lsp.buf.format, { remap = false })
+
--- lua/core/plugin_config/prettier.lua
+++ lua/core/plugin_config/prettier.lua
@@ -18,26 +18,4 @@ prettier.setup({
     "yaml",
     "php"
   },
-  cli_options = {
-    arrow_parens = "always",
-    bracket_same_line = false,
-    bracket_spacing = true,
-    cursor_offset = -1,
-    editorconfig = false,
-    embedded_language_formatting = "auto",
-    end_of_line = "lf",
-    html_whitespace_sensitivity = "css",
-    insert_pragma = false,
-    jsx_single_quote = false,
-    print_width = 120,
-    prose_wrap = "preserve",
-    quote_props = "as-needed",
-    require_pragma = "false",
-    semi = true,
-    single_attribute_per_line = true,
-    single_quote = true,
-    tabWidth = 2,
-    trailingComa = "all",
-    use_tabs = true
-  }
 })
